e19e3b9a9f9e55a29c160016c2fcc87abe341904
나평근 <skvudrms54@gmail.com>
2019-11-22 20:08:57 +0900

    create project
    DBConfing는 ignore 설정함

---
+++ pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.itbuddy.codehouse.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {DBConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/codehouse"};
+	}
+	
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);	
+		
+		return new Filter[] {filter};
+	}
+
+	
+}
