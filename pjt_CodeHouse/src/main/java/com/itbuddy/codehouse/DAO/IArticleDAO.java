9555139671e8a60ca6a84b89a3befd28a86f9eee
나평근 <skvudrms54@gmail.com>
2019-11-23 14:41:18 +0900

    Add IArticleDAO.xml

--- pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/DAO/IArticleDAO.java
+++ pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/DAO/IArticleDAO.java
@@ -1,5 +1,16 @@
 package com.itbuddy.codehouse.DAO;
 
-public class IArticleDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.itbuddy.codehouse.DTO.Article;
+
+
+public interface IArticleDAO {
+	// Artcle을 Parm기준으로 정렬하여 가져온다.
+	public List<Article> selectArticleListOrderByParmDesc( @Param("start") int start, 
+															@Param("count") int count, 
+															@Param("param") String param); 
+	
 }
