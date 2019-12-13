90691af07a37c9aacb2f5a31aaf44bd49506d0e0
나평근 <skvudrms54@gmail.com>
2019-12-13 17:13:41 +0900

    edit pm.xml TopNavServiceTest

--- pjt_CodeHouse/src/test/java/com/itbuddy/codehouse/Service/TopNavServiceTest.java
+++ pjt_CodeHouse/src/test/java/com/itbuddy/codehouse/Service/TopNavServiceTest.java
@@ -4,6 +4,7 @@ import com.itbuddy.codehouse.DTO.Member;
 import com.itbuddy.codehouse.config.DBConfig;
 import com.itbuddy.codehouse.service.ITopNavService;
 
+import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,16 +29,17 @@ public class TopNavServiceTest {
     
     @Test
     public void loginTest(){
-        Member input = new Member();
-        input.setMem_id("billip");
-        input.setMem_pw("1234");
-        boolean login = TopNavService.login(input);
-
-        if(login){
-            LOGGER.info("로그인 성공");
-        }else{
-            LOGGER.info("로그인 실패");
-        }
+//        Member input = new Member();
+//        HttpSession session = new HttpSession();
+//        input.setMem_id("billip");
+//        input.setMem_pw("1234");
+//        boolean login = TopNavService.login(input, session);
+//
+//        if(login){
+//            LOGGER.info("로그인 완료");
+//        }else{
+//            LOGGER.info("로그인 실패");
+//        }
 
 
         
