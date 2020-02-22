ccd307ff706e2cee85a1a27146feb585ae75d40e
나평근 <skvudrms54@gmail.com>
2020-02-22 15:45:49 +0900

    Test for Cobertura

--- pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/controller/UploadController.java
+++ pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/controller/UploadController.java
@@ -8,8 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import com.itbuddy.codehouse.service.IUploadService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/serviceImpl/UploadService.java
+++ pjt_CodeHouse/src/main/java/com/itbuddy/codehouse/serviceImpl/UploadService.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
